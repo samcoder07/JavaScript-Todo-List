dcf5647075663fe8414f090b00c385f505c8b4d3
samarthbluelupin <145488981+samcoder07@users.noreply.github.com>
2024-01-08 09:00:33 +0530

    checkin

--- main.js
+++
@@ -1,4 +0,0 @@
-const addbtn=document.querySelector("#add-btn");
-const newTaskInput=document.querySelector("#wrapper-input");
-const taskcontainer=document.querySelector("#tasks");
-const error=document.querySelector("error");
